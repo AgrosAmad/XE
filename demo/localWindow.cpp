786b4a2b0f6fae35b53990fd1146fb8697c61634
AgrosAmad <jorge.amadorherrera@kaust.edu.sa>
2021-07-07 15:44:42 +0300

    new tutorial

--- demo/localWindow.cpp
+++ demo/localWindow.cpp
@@ -18,14 +18,16 @@ void LocalWindow::initializeScene()
 		auto& mainShaderProgram = spm.createShaderProgram("main");
 		mainShaderProgram.addShaderToProgram(sm.getVertexShader("figures"));
 		mainShaderProgram.addShaderToProgram(sm.getFragmentShader("figures"));
-
-		House = std::make_unique<static_meshes_3D::House>();
-		Snowground = std::make_unique<static_meshes_3D::SnowCoveredPlainGround>(true, true, true);
-
-		HUD = std::make_unique<HUD010>(*this);
 		spm.linkAllPrograms();
-
 		SamplerManager::getInstance().createSampler("main", MAG_FILTER_BILINEAR, MIN_FILTER_TRILINEAR);
+
+		TextureManager::getInstance().loadTexture2D("diamond", "../textures/diamond.png");
+		TextureManager::getInstance().loadTexture2D("metal", "../textures/metal.png");
+		TextureManager::getInstance().loadTexture2D("ice", "../textures/ice.png");
+
+		Pyramid = std::make_unique<static_meshes_3D::Pyramid>(true, true, false);
+		Torus = std::make_unique<static_meshes_3D::Torus>(50, 50, 3.0f, 1.5f, true, true, false);
+		PlainGround = std::make_unique<static_meshes_3D::PlainGround>(true, true, false);
 	}
 	catch (const std::runtime_error& ex)
 	{
@@ -47,27 +49,43 @@ void LocalWindow::renderScene()
 
 	// Set matrices in matrix manager
 	mm.setProjectionMatrix(getProjectionMatrix());
-	mm.setOrthoProjectionMatrix(getOrthoProjectionMatrix());
 	mm.setViewMatrix(Camera.getViewMatrix());
 
-	Snowground->render();
-
-	// Render all houses
+	// Set up some common properties in the main shader program
 	auto& mainProgram = spm.getShaderProgram("main");
 	mainProgram.useProgram();
 	SamplerManager::getInstance().getSampler("main").bind();
 	mainProgram[ShaderConstants::projectionMatrix()] = getProjectionMatrix();
 	mainProgram[ShaderConstants::viewMatrix()] = Camera.getViewMatrix();
+	mainProgram[ShaderConstants::modelMatrix()] = glm::mat4(1.0f);
 	mainProgram[ShaderConstants::color()] = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f);
 	mainProgram[ShaderConstants::sampler()] = 0;
 
-	for (auto& houseTransformation : houseTransformations)
-	{
-		House->render(houseTransformation.position, houseTransformation.angle);
-	}
+	// Render icy ground
+	TextureManager::getInstance().getTexture("ice").bind(0);
+	PlainGround->render();
 
-	// Render HUD
-	HUD->renderHUD();
+	for (const auto& position : objectPositions)
+	{
+		// Render diamond pyramid on bottom
+		const auto pyramidSize = 10.0f;
+		auto posModelMatrix = glm::translate(glm::mat4(1.0f), position);
+		auto model = glm::translate(posModelMatrix, glm::vec3(0.0f, pyramidSize / 2.0f, 0.0f));
+		model = glm::scale(model, glm::vec3(pyramidSize, pyramidSize, pyramidSize));
+		mainProgram[ShaderConstants::modelMatrix()] = model;
+
+		TextureManager::getInstance().getTexture("diamond").bind(0);
+		Pyramid->render();
+
+		// Render metal torus on top of the pyramid
+		const auto torusOffset = pyramidSize + Torus->getMainRadius() + Torus->getTubeRadius();
+		model = glm::translate(posModelMatrix, glm::vec3(0.0f, torusOffset, 0.0f));
+		model = glm::rotate(model, rotationAngleRad, glm::vec3(0.0f, 1.0f, 0.0f));
+		mainProgram[ShaderConstants::modelMatrix()] = model;
+
+		TextureManager::getInstance().getTexture("metal").bind(0);
+		Torus->render();
+	}
 }
 
 void LocalWindow::updateScene()
@@ -82,11 +100,6 @@ void LocalWindow::updateScene()
 		setVerticalSynchronization(!isVerticalSynchronizationEnabled());
 	}
 
-	if (keyPressedOnce(GLFW_KEY_F4))
-	{
-		HUD->toggleBlending();
-	}
-
 	int posX, posY, width, height;
 	glfwGetWindowPos(getWindow(), &posX, &posY);
 	glfwGetWindowSize(getWindow(), &width, &height);
@@ -99,8 +112,11 @@ void LocalWindow::updateScene()
 
 	std::string windowTitleWithFPS = "X Engine: " + std::to_string(getFPS()) 
 	    + ", VSync: " + (isVerticalSynchronizationEnabled() ? "On" : "Off") + " (Press F3 to toggle)"
-		+ +", Blending: " + (HUD->isBlendingEnabled() ? "On" : "Off") + " (Press F4 to toggle)";
+		+ " (Press F3 to toggle)";
 	glfwSetWindowTitle(getWindow(), windowTitleWithFPS.c_str());
+
+	// Update rotation angle
+	rotationAngleRad += sof(glm::radians(135.0f));
 }
 
 void LocalWindow::releaseScene()
@@ -110,8 +126,8 @@ void LocalWindow::releaseScene()
 	TextureManager::getInstance().clearTextureCache();
 	SamplerManager::getInstance().clearSamplerCache();
 
-	House.reset();
-	Snowground.reset();
-	//HUD.reset();
+	Pyramid.reset();
+	Torus.reset();
+	PlainGround.reset();
 
 }
--- demo/localWindow.h
+++ demo/localWindow.h
@@ -22,8 +22,9 @@
 #include "staticGeometry.h"
 
 // Meshes
-#include "geometry/static_meshes_3D/house.h"
-#include "geometry/static_meshes_3D/snowCoveredPlainGround.h"
+#include "geometry/static_meshes_3D/plainGround.h"
+#include "geometry/static_meshes_3D/primitives/pyramid.h"
+#include "geometry/static_meshes_3D/primitives/torus.h"
 
 namespace XE
 {
@@ -37,39 +38,27 @@ namespace XE
     private:
 
         // Rendering
-        std::unique_ptr<static_meshes_3D::House> House;
-        std::unique_ptr<static_meshes_3D::SnowCoveredPlainGround> Snowground;
-        std::unique_ptr<HUD010> HUD;
+        std::unique_ptr<static_meshes_3D::Pyramid> Pyramid;
+        std::unique_ptr<static_meshes_3D::Torus> Torus;
+        std::unique_ptr<static_meshes_3D::PlainGround> PlainGround;
 
         // Camera
         FlyingCamera Camera = FlyingCamera(glm::vec3(0.0f, 8.0f, 20.0f), glm::vec3(0.0f, 8.0f, 19.0f), glm::vec3(0.0f, 1.0f, 0.0f), 50);
 
         // Transform scene
-        struct HouseTransformation
+        float rotationAngleRad = 0.0f;
+        std::vector<glm::vec3> objectPositions
         {
-            glm::vec3 position;
-            float angle;
-        };
-
-        std::vector<HouseTransformation> houseTransformations
-        {
-            {glm::vec3(-140, 0, 94), glm::radians(90.0f)},
-            {glm::vec3(-140, 0, 64), glm::radians(90.0f)},
-            {glm::vec3(-140, 0, 34), glm::radians(90.0f)},
-            {glm::vec3(-140, 0, 4), glm::radians(90.0f)},
-            {glm::vec3(-97, 0, 18), glm::radians(-90.0f)},
-            {glm::vec3(-97, 0, 48), glm::radians(-90.0f)},
-            {glm::vec3(-97, 0, 78), glm::radians(-90.0f)},
-            {glm::vec3(-63, 0, -14), glm::radians(180.0f)},
-            {glm::vec3(-33, 0, -14), glm::radians(180.0f)},
-            {glm::vec3(-3, 0, -14), glm::radians(180.0f)},
-            {glm::vec3(27, 0, -14), glm::radians(180.0f)},
-            {glm::vec3(-75, 0, -64), glm::radians(0.0f)},
-            {glm::vec3(-45, 0, -64), glm::radians(0.0f)},
-            {glm::vec3(-15, 0, -64), glm::radians(0.0f)},
-            {glm::vec3(15, 0, -64), glm::radians(0.0f)},
-            {glm::vec3(45, 0, -64), glm::radians(0.0f)},
-            {glm::vec3(-121, 0, -54), glm::radians(45.0f)}
+            glm::vec3(-30.0f, 0.0f, -50.0f),
+            glm::vec3(-30.0f, 0.0f, -25.0f),
+            glm::vec3(-30.0f, 0.0f, 0.0f),
+            glm::vec3(-30.0f, 0.0f, 25.0f),
+            glm::vec3(-30.0f, 0.0f, 50.0f),
+            glm::vec3(30.0f, 0.0f, -50.0f),
+            glm::vec3(30.0f, 0.0f, -25.0f),
+            glm::vec3(30.0f, 0.0f, 0.0f),
+            glm::vec3(30.0f, 0.0f, 25.0f),
+            glm::vec3(30.0f, 0.0f, 50.0f)
         };
     };
 } // namespace XE
